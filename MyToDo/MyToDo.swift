37f70592bdd68f90f22ed9ac294888ede466d0b7
Alex Shook <alex.shook@salido.com>
2017-11-30 15:12:33 -0500

    Initial commit

---
+++ MyToDo/MyToDo.swift
@@ -0,0 +1,109 @@
+//
+//  MyToDo.swift
+//  MyToDo
+//
+//  Created by Alex Shook on 11/30/17.
+//  Copyright © 2017 Alex Shook. All rights reserved.
+//
+
+import Foundation
+
+class ToDoItem: NSObject, NSCoding
+{
+    var title: String
+    var done: Bool
+    
+    public init(title: String)
+    {
+        self.title = title
+        self.done = false
+    }
+
+    required init?(coder aDecoder: NSCoder)
+    {
+        if let title = aDecoder.decodeObject(forKey: "title") as? String
+        {
+            self.title = title
+        }
+        else
+        {
+            return nil
+        }
+        
+        if aDecoder.containsValue(forKey: "done")
+        {
+            self.done = aDecoder.decodeBool(forKey: "done")
+        }
+        else
+        {
+            return nil
+        }
+    }
+    
+    func encode(with aCoder: NSCoder)
+    {
+        aCoder.encode(self.title, forKey: "title")
+        aCoder.encode(self.done, forKey: "done")
+    }
+
+}
+
+extension ToDoItem
+{
+
+    public class func getMockData() -> [ToDoItem]
+    {
+        return [
+            ToDoItem(title: "Swift"),
+            ToDoItem(title: "Objective-C"),
+            ToDoItem(title: "Elm"),
+            ToDoItem(title: "Elixir")
+        ]
+    }
+}
+
+extension Collection where Iterator.Element == ToDoItem
+{
+    private static func persistencePath() -> URL?
+    {
+        let url = try? FileManager.default.url(
+            for: .applicationSupportDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: true)
+        
+        return url?.appendingPathComponent("todoitems.bin")
+    }
+    
+    func writeToPersistence() throws
+    {
+        if let url = Self.persistencePath(), let array = self as? NSArray
+        {
+            let data = NSKeyedArchiver.archivedData(withRootObject: array)
+            try data.write(to: url)
+        }
+        else
+        {
+            throw NSError(domain: "com.example.MyToDo", code: 10, userInfo: nil)
+        }
+    }
+    
+    static func readFromPersistence() throws -> [ToDoItem]
+    {
+        if let url = persistencePath(), let data = (try Data(contentsOf: url) as Data?)
+        {
+            if let array = NSKeyedUnarchiver.unarchiveObject(with: data) as? [ToDoItem]
+            {
+                return array
+            }
+            else
+            {
+                throw NSError(domain: "com.example.MyToDo", code: 11, userInfo: nil)
+            }
+        }
+        else
+        {
+            throw NSError(domain: "com.example.MyToDo", code: 12, userInfo: nil)
+        }
+    }
+}
--- MyToDo/ViewController.swift
+++ MyToDo/ViewController.swift
@@ -8,11 +8,132 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    private var todoItems = [ToDoItem]()
+    
+    @objc
+    public func applicationDidEnterBackground(_ notification: NSNotification)
+    {
+        do
+        {
+            try todoItems.writeToPersistence()
+        }
+        catch let error
+        {
+            NSLog("Error writing to persistence: \(error)")
+        }
+    }
+    
+    func didTapAddItemButton(_ sender: UIBarButtonItem)
+    {
+        let alert = UIAlertController(
+            title: "New to-do item",
+            message: "Insert the title of the new to-do item:",
+            preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: nil)
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            if let title = alert.textFields?[0].text
+            {
+                self.addNewToDoItem(title: title)
+            }
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func addNewToDoItem(title: String)
+    {
+        let newIndex = todoItems.count
+        todoItems.append(ToDoItem(title: title))
+        
+        tableView.insertRows(at: [IndexPath(row: newIndex, section: 0)], with: .top)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if indexPath.row < todoItems.count
+        {
+            todoItems.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .top)
+        }
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return todoItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_todo", for: indexPath)
+        
+        if indexPath.row < todoItems.count
+        {
+            let item = todoItems[indexPath.row]
+            cell.textLabel?.text = item.title
+            
+            let accessory: UITableViewCellAccessoryType = item.done ? .checkmark : .none
+            cell.accessoryType = accessory
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row < todoItems.count
+        {
+            let item = todoItems[indexPath.row]
+            item.done = !item.done
+            
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "To-Do"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ViewController.didTapAddItemButton(_:)))
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(UIApplicationDelegate.applicationDidEnterBackground(_:)),
+            name: NSNotification.Name.UIApplicationDidEnterBackground,
+            object: nil)
+        do
+        {
+            self.todoItems = try [ToDoItem].readFromPersistence()
+        }
+        catch let error as NSError
+        {
+            if error.domain == NSCocoaErrorDomain && error.code == NSFileReadNoSuchFileError
+            {
+                let alert = UIAlertController(
+                    title: "Error",
+                    message: "Could not load the to-do items!",
+                    preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+                
+                NSLog("Error loading from persistence:\(error)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
