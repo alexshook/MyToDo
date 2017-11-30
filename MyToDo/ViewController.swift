54bf4a2379675b17007fa8cead3e66529dbd7f3a
Alex Shook <alex.shook@salido.com>
2017-11-30 13:41:27 -0500

    Initial Commit

---
+++ MyToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyToDo
+//
+//  Created by Alex Shook on 11/30/17.
+//  Copyright © 2017 Alex Shook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
